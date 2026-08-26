c960dbe3262bfc8cf01f00edef74b9d59cf27cd3
MaybeWeDead <hktline@proton.me>
2026-08-26 11:56:51 +0300

    fix

---
+++ build-arm64/CMakeFiles/vexland.dir/cmake_clean.cmake
@@ -0,0 +1,38 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vexland.dir/link.d"
+  "CMakeFiles/vexland.dir/src/Compositor.cpp.o"
+  "CMakeFiles/vexland.dir/src/Compositor.cpp.o.d"
+  "CMakeFiles/vexland.dir/src/config/ConfigManager.cpp.o"
+  "CMakeFiles/vexland.dir/src/config/ConfigManager.cpp.o.d"
+  "CMakeFiles/vexland.dir/src/desktop/Workspace.cpp.o"
+  "CMakeFiles/vexland.dir/src/desktop/Workspace.cpp.o.d"
+  "CMakeFiles/vexland.dir/src/desktop/state/FocusState.cpp.o"
+  "CMakeFiles/vexland.dir/src/desktop/state/FocusState.cpp.o.d"
+  "CMakeFiles/vexland.dir/src/desktop/view/Window.cpp.o"
+  "CMakeFiles/vexland.dir/src/desktop/view/Window.cpp.o.d"
+  "CMakeFiles/vexland.dir/src/keybinds/Bind.cpp.o"
+  "CMakeFiles/vexland.dir/src/keybinds/Bind.cpp.o.d"
+  "CMakeFiles/vexland.dir/src/keybinds/Key.cpp.o"
+  "CMakeFiles/vexland.dir/src/keybinds/Key.cpp.o.d"
+  "CMakeFiles/vexland.dir/src/keybinds/Manager.cpp.o"
+  "CMakeFiles/vexland.dir/src/keybinds/Manager.cpp.o.d"
+  "CMakeFiles/vexland.dir/src/layout/algorithm/Algorithm.cpp.o"
+  "CMakeFiles/vexland.dir/src/layout/algorithm/Algorithm.cpp.o.d"
+  "CMakeFiles/vexland.dir/src/layout/space/Space.cpp.o"
+  "CMakeFiles/vexland.dir/src/layout/space/Space.cpp.o.d"
+  "CMakeFiles/vexland.dir/src/layout/target/Target.cpp.o"
+  "CMakeFiles/vexland.dir/src/layout/target/Target.cpp.o.d"
+  "CMakeFiles/vexland.dir/src/layout/target/WindowTarget.cpp.o"
+  "CMakeFiles/vexland.dir/src/layout/target/WindowTarget.cpp.o.d"
+  "CMakeFiles/vexland.dir/src/main.cpp.o"
+  "CMakeFiles/vexland.dir/src/main.cpp.o.d"
+  "CMakeFiles/vexland.dir/src/output/MonitorState.cpp.o"
+  "CMakeFiles/vexland.dir/src/output/MonitorState.cpp.o.d"
+  "vexland"
+  "vexland.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vexland.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
